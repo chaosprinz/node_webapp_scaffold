16537f5ad06ab8b15a1399906625bc5e13470063
Siggi Duenkel <chaosprinz76@googlemail.com>
2016-04-30 22:07:34 +0200

    adding tests for the callback of the homepage-route

---
+++ test/routes/indexRoutesSpec.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.should()
+chai.use(sinonChai)
+
+process.env.CONFIGS = process.cwd() + "/test/routes/fixtures/conf"
+
+
+describe('home',function(){
+
+  const routeStringsConfig = require("./fixtures/conf/strings")
+
+  let routeFile = process.cwd() + "/routes/index"
+  let indexRoutes = require(routeFile)
+
+  let reloadRouteFile = function(){
+    delete require.cache[require.resolve(routeFile)]
+    indexRoutes = require(routeFile)
+  }
+
+  let req = {}; let res = {}
+  let passedToPug
+
+  beforeEach(function(){
+    process.env.NODE_ENV = "development"
+    res.render = sinon.spy()
+    passedToPug = {
+      title: routeStringsConfig.app.name,
+      description: routeStringsConfig.app.description,
+      livereload: true
+    }
+    reloadRouteFile()
+  })
+
+  it('should inject livereload-script based on NODE_ENV', function(){
+    indexRoutes.home(req, res)
+    res.render.should.have.been.calledWith('index', passedToPug)
+
+    process.env.NODE_ENV = "production"
+    reloadRouteFile()
+    passedToPug.livereload = false
+    indexRoutes.home(req, res)
+    res.render.should.have.been.calledWith('index', passedToPug)
+  })
+
+  it('should respond with set title and description', function(){
+    indexRoutes.home(req, res)
+    res.render.should.have.been.calledWith('index',passedToPug)
+  })
+})
